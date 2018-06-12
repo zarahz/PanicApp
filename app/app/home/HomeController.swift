083f8dfa250af5c9f37eac8b4398f7d8b025b9ca
Katharina <katharina.bause@yahoo.de>
2018-06-12 12:37:34 +0200

    [KB] data in array and timer finished
    BUGGY: after starting startbreathing the second time, there are several issues. the index behaves strangely

--- app/app/home/HomeController.swift
+++ app/app/home/HomeController.swift
@@ -16,21 +16,27 @@ class HomeController: UIViewController {
     var myView = UIView(frame: CGRect(x: 0 , y: 0 , width: 1000 , height: 1000 ))
     var motionManager = CMMotionManager()
     var counterStart = 0
-    var counterBreath = 0
+    var counterBreath = 1
     var breathing = false
     var timer5 : Timer!
-
+    var breatheIn = true
+    var accData = Array(repeating: 0.0, count: 10)
+    var singleData = 0.0
+    var index = 0
     
+    
+    //MARK: build up the view
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
         // Do any additional setup after loading the view, typically from a nib.
-
+        
         self.view.addSubview(myView)
         let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.checkActionStart))
         self.myView.addGestureRecognizer(gesture)
     }
     
+    //MARK: tap on screen to start the breathing function
     @objc func checkActionStart(sender : UITapGestureRecognizer) {
         counterStart=counterStart+1;
         if counterStart % 2 == 1 {
@@ -44,76 +50,109 @@ class HomeController: UIViewController {
             print(breathing)
         }
     }
-
+    
     
     //MARK: breathing
     func startBreathing(){
-        // audio ausgabe für start und ende und zwischendurch
+        // TODO: audio ausgabe für start und ende und zwischendurch
         motionManager.accelerometerUpdateInterval = 0.4
         print("wir sind in startBreathing")
         //? --> kann wert haben oder nil. ! muss einen wert haben
-
+        
         
         timer5 = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(runTimedCode5), userInfo: nil, repeats: true)
-
+        
         let date0 = Date()
         var date1 = date0.timeIntervalSinceNow
-        var breathIn = true
+        
         
         //UM BACKSALSH ZU MACHEN alt SHIFT 7!!!!!
         /*while (breathing){
-        while date1 > -4.0 && breathing{
-            date1 = date0.timeIntervalSinceNow
-            breathIn = true;
-            print("breathing in \(date1)")
-        }
-        while date1 > -10.0 && breathing {
-            date1 = date0.timeIntervalSinceNow
-            breathIn = false
-            print("breathing out\(date1)")
-        }
-        date1 = 0.0
-        
-        }*/
+         while date1 > -4.0 && breathing{
+         date1 = date0.timeIntervalSinceNow
+         breathIn = true;
+         print("breathing in \(date1)")
+         }
+         while date1 > -10.0 && breathing {
+         date1 = date0.timeIntervalSinceNow
+         breathIn = false
+         print("breathing out\(date1)")
+         }
+         date1 = 0.0
+         
+         }*/
         //timeintervalsincenow
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     func getData(){
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!){ (data, error) in
             if let myData = data{
                 print(myData)
+                self.singleData = myData.acceleration.x
             }
         }
     }
+    //if it looks stupid but works, it is not stupid.
+    //546123
     @objc func runTimedCode5(){
+        self.index = 0
+        //check if the user tapped a second time in order to stop the exercise
         if breathing {
-            print("5")//546123
-            counterBreath += 1
+            
+            //check if it is a breathe in or a breathe out
             if counterBreath%2 == 1{
-                print("einatmen")// x 0.05 - 0.07; y -0.11 - -0.15; z -0.98 - -1.0
+                breatheIn = true
+                //print("einatmen")// x 0.05 - 0.07; y -0.11 - -0.15; z -0.98 - -1.0
             } else {
-                print("ausatmen")// x0.06 - 0.02; y -0.12
+                breatheIn = false
+                //print("ausatmen")// x0.06 - 0.02; y -0.12
             }
-                getData()
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            counterBreath += 1
+            
+            print("5")
+            self.breathingData(index: index)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 print("5+1")
-                    self.getData()
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                print("5+1")
-                    self.getData()
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-                print("5+1")
-                    self.getData()
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-                print("5+1")
-                    self.getData()
+                self.breathingData(index: self.index)
             }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                print("5+1")
+                self.breathingData(index: self.index)
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                print("5+1")
+                self.breathingData(index: self.index)
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                if self.breatheIn {
+                    self.breatheIn = false
+                }
+                print("5+1")
+                self.breathingData(index: self.index)
+                
+            }
+            
         }
+    }
+    func breathingData (index: Int) {
+        print(index)
+        print(self.breatheIn)
+        self.getData()
+        if(self.breatheIn){
+            self.accData[index] = self.singleData
+        } else{
+            self.accData[index+5] = self.singleData
+        }
+        self.index += 1
+        self.index = self.index%5
     }
 }
+
+//data while breathing in: x-Achse zunehmend
+//data while breathing out: x-Achse abnehmend
