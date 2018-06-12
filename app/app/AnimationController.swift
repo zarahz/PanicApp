5fd5722d54f7a151f76663dba83bc09710e8d92f
MW <marianne@westenthanner.de>
2018-06-12 13:11:34 +0200

    [MW] removed animation, new animation controller

---
+++ app/app/AnimationController.swift
@@ -0,0 +1,29 @@
+//
+//  AnimationController.swift
+//  app
+//
+//  Created by Marianne on 12.06.18.
+//  Copyright © 2018 Zarah Zahreddin. All rights reserved.
+//
+
+import UIKit
+
+class AnimationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        
+        // Animate
+        
+        
+    }
+    
+    func touchJellyfish() {
+        
+    }
+    
+    func animateJellyfish() {
+        
+    }
+}
