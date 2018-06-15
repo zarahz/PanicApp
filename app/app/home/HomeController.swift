88e2458b87e2726a6640e85bf8cf38edb2b5c81e
Katharina <katharina.bause@yahoo.de>
2018-06-15 15:05:44 +0200

    data average
    getting data completed

--- app/app/home/HomeController.swift
+++ app/app/home/HomeController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import CoreMotion
+import AVFoundation
 
 
 
@@ -20,9 +21,11 @@ class HomeController: UIViewController {
     var breathing = false
     var timer5 : Timer?
     var breatheIn = true
-    var accData = Array(repeating: 0.0, count: 10)
+    var accData = [Double]()
+    var accData10 = Array(repeating: 0.0, count: 10)
     var singleData = 0.0
     var index = 0
+    var audioPlayer : AVAudioPlayer = AVAudioPlayer()
     
     
     //MARK: build up the view
@@ -34,6 +37,7 @@ class HomeController: UIViewController {
         self.view.addSubview(myView)
         let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.checkActionStart))
         self.myView.addGestureRecognizer(gesture)
+
     }
     
     //MARK: tap on screen to start the breathing function
@@ -48,9 +52,11 @@ class HomeController: UIViewController {
             breathing = false
             print("Touched to end")
             print(breathing)
+            
             if timer5 != nil {
                 timer5?.invalidate()
                 timer5 = nil
+                
             }
         }
     }
@@ -59,10 +65,12 @@ class HomeController: UIViewController {
     //MARK: breathing
     func startBreathing(){
         // TODO: audio ausgabe für start und ende und zwischendurch
-        motionManager.accelerometerUpdateInterval = 0.4
+        // mehr Werte --> Durchschnitt bilden und abfragen
+        // startUpdate Methode
+        motionManager.accelerometerUpdateInterval = 0.2
         print("wir sind in startBreathing")
         //? --> kann wert haben oder nil. ! muss einen wert haben
-        
+        getData()
         
         timer5 = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(runTimedCode5), userInfo: nil, repeats: true)
         
@@ -96,11 +104,21 @@ class HomeController: UIViewController {
     func getData(){
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!){ (data, error) in
             if let myData = data{
-                print(myData)
-                self.singleData = myData.acceleration.x
+                self.accData.append(myData.acceleration.x)
             }
         }
     }
+    func getAverage() -> Double{
+        var tmp = 0.0
+        for i in 0...accData.count-1{
+            tmp = tmp + accData[i]
+        }
+        print(tmp/Double(accData.count))
+        tmp = tmp/Double(accData.count)
+        accData.removeAll()
+        return tmp
+    }
+    
     //if it looks stupid but works, it is not stupid.
     //546123
     @objc func runTimedCode5(){
@@ -118,6 +136,7 @@ class HomeController: UIViewController {
             }
             counterBreath += 1
             
+            
             print("5")
             self.breathingData(index: index)
             
@@ -134,12 +153,12 @@ class HomeController: UIViewController {
                 self.breathingData(index: self.index)
             }
             DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+
+                print("5+1")
+                self.breathingData(index: self.index)
                 if self.breatheIn {
                     self.breatheIn = false
                 }
-                print("5+1")
-                self.breathingData(index: self.index)
-                
             }
             
         }
@@ -147,14 +166,34 @@ class HomeController: UIViewController {
     func breathingData (index: Int) {
         print(index)
         print(self.breatheIn)
-        self.getData()
-        if(self.breatheIn){
-            self.accData[index] = self.singleData
-        } else{
-            self.accData[index+5] = self.singleData
+        
+        
+        if(self.breatheIn && index < 4){
+            self.accData10[index] = getAverage()
+            //beim einatmen ist x steigend. es soll also dem nutzer melden, wenn es fallend ist, um ihn zu korrigieren
+            if index > 0{
+                if self.accData10[index] <= self.accData10[index-1] {
+                //give out some sound
+                }
+            }
+            
+            //breathe out --> ab letztem index bei breathe in und dann alles in breathein false
+        } else if self.breatheIn && index == 4{
+            self.accData10[index] = getAverage()
+            
+            //breathe out
+        } else if !breatheIn {
+            self.accData10[index+5] = getAverage()
+            //beim ausatmen ist x fallend. es soll also dem nutzer melden, wenn es steigend ist, um ihn zu korrigieren.
+            if self.accData10[index+5] >= self.accData10[index+4]{
+                //give out some sound
+            }
         }
         self.index += 1
         self.index = self.index%5
+        if(!breathing && index == 4){
+            motionManager.stopAccelerometerUpdates()
+        }
     }
 }
 
