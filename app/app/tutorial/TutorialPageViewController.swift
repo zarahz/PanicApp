de14e9735b95c83345d29514cd744e8f29ba3df0
Paula Wikidal <Paula@paulas-mbp.fritz.box>
2018-06-28 22:38:45 +0200

    [PW] Dynamic tutorial screens

--- app/app/tutorial/TutorialPageViewController.swift
+++ app/app/tutorial/TutorialPageViewController.swift
@@ -19,7 +19,7 @@ class TutorialPageViewController: UIPageViewController, UIPageViewControllerData
         delegate = self
         
         //load viewControllers
-        for i in 0...2 {
+        for i in 0...3 {
             let vc = UIStoryboard(name: "Main", bundle: nil) .
                 instantiateViewController(withIdentifier:"tutorial\(i)")
             viewControllersList.append(vc)
